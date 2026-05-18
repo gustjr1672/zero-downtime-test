1b2fb42da2b0d6d958bf9031951fc49a02409047
gustjr1672 <gustjr2332@naver.com>
2026-05-18 17:03:11 +0900

    mvc구조 추가

---
+++ ZeroDowntimeApi/Controller/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Data.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZeroDowntimeApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        // 5초 동안 여기서 대기하게 됩니다.
+        var result = await _userService.GetFormattedUserNameAsync(id);
+
+        return Ok(new
+        {
+            Message = "성공",
+            UserId = id,
+            Data = result
+        });
+    }
+}
