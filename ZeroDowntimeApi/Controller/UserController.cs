231b3227c215ecaf9b14c8f888578411ddfb54b0
gustjr1672 <gustjr2332@naver.com>
2026-05-28 15:19:17 +0900

    버전 업

--- ZeroDowntimeApi/Controller/UserController.cs
+++ ZeroDowntimeApi/Controller/UserController.cs
@@ -21,7 +21,7 @@ public class UserController : ControllerBase
 
         return Ok(new
         {
-            Version = $"{delay}초 지연 버전 V1)",
+            Version = $"{delay}초 지연 버전 V2)",
             Message = "성공",
             UserId = id,
             Data = result
@@ -35,7 +35,7 @@ public class UserController : ControllerBase
 
         return Ok(new
         {
-            Version = "quick (즉시) V1",
+            Version = "quick (즉시) V2",
             Message = "성공",
             UserId = id,
             Data = result
